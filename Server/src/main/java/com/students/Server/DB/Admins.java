f2a81fe300226c4743dd6ba182dc6f8e31aab82a
revoly afanasev <rewoly@mail.ru>
2024-12-09 21:31:36 +0900

    Работа с сущностями

---
+++ Server/src/main/java/com/students/Server/DB/Admins.java
@@ -0,0 +1,39 @@
+package com.students.Server.DB;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+/**
+ * Класс сущность для взаимодействия с таблицей в БД "admins"
+ */
+@Setter
+@Getter
+@Entity
+@Table(name = "admins")
+public class Admins {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "surname")
+    private String surname;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "patronymic")
+    private String patronymic;
+    @Column(name = "login")
+    private String login;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "position")
+    private String position;
+    public Admins(){};
+    public Admins(String surname, String name, String patronymic, String login, String password, String position) {
+        this.surname = surname;
+        this.name = name;
+        this.patronymic = patronymic;
+        this.login = login;
+        this.password = password;
+        this.position = position;
+    }
+}
