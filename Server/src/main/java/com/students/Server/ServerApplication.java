405d1337da50cb3fd3787240871080017752ae71
revoly afanasev <rewoly@mail.ru>
2024-12-10 22:25:44 +0900

    Настройка подключения к БД

--- Server/src/main/java/com/students/Server/ServerApplication.java
+++ Server/src/main/java/com/students/Server/ServerApplication.java
@@ -2,7 +2,9 @@ package com.students.Server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * Точка запуска приложения
+ */
 @SpringBootApplication
 public class ServerApplication {
 	public static void main(String[] args) {
