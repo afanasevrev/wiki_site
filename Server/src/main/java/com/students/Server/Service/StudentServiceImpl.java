d7781dc85e524557d2799edeb53f45b3935058c5
revoly afanasev <rewoly@mail.ru>
2024-12-12 22:32:38 +0900

    Работа с репозиториями

--- Server/src/main/java/com/students/Server/Service/StudentServiceImpl.java
+++ Server/src/main/java/com/students/Server/Service/StudentServiceImpl.java
@@ -1,7 +1,8 @@
 package com.students.Server.Service;
 
+import com.students.Server.Repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 public class StudentServiceImpl implements StudentService {
     @Autowired
-    private StudentService studentService;
+    private StudentRepository studentRepository;
 }
