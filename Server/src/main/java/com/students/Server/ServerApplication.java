e860a096ac0ee16d0acbee830948f34931e4ff6f
revoly afanasev <rewoly@mail.ru>
2024-12-12 22:35:15 +0900

    Работа с репозиториями

--- Server/src/main/java/com/students/Server/ServerApplication.java
+++ Server/src/main/java/com/students/Server/ServerApplication.java
@@ -5,13 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 /**
  * Точка запуска приложения
  */
 @SpringBootApplication
 @EnableAutoConfiguration
-@ComponentScan
 public class ServerApplication {
 	@Bean
 	public AdminsService photoService() {
