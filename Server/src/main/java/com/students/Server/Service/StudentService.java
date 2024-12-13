73d3f64bd8ff3e574bf6bab2272c2bd898747e3f
revoly afanasev <rewoly@mail.ru>
2024-12-13 22:29:52 +0900

    Работа с сущностями

--- Server/src/main/java/com/students/Server/Service/StudentService.java
+++ Server/src/main/java/com/students/Server/Service/StudentService.java
@@ -1,9 +1,17 @@
 package com.students.Server.Service;
 
+import com.students.Server.Entity.StudentEntity;
 import com.students.Server.Repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 @Service
 public interface StudentService {
-
+    String createStudent(StudentEntity entity);
+    List<StudentEntity> readStudents();
+    StudentEntity readStudent(Long id);
+    String deleteStudent(Long id);
+    String updateStudent(StudentEntity entity, Long id);
 }
--- Server/src/main/java/com/students/Server/Service/StudentServiceImpl.java
+++ Server/src/main/java/com/students/Server/Service/StudentServiceImpl.java
@@ -1,8 +1,39 @@
 package com.students.Server.Service;
 
+import com.students.Server.Entity.StudentEntity;
 import com.students.Server.Repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+
 public class StudentServiceImpl implements StudentService {
     @Autowired
     private StudentRepository studentRepository;
+    @Override
+    public String createStudent(StudentEntity entity) {
+        studentRepository.save(entity);
+        return "Студент добавлен в БД";
+    }
+    @Override
+    public List<StudentEntity> readStudents() {
+        return studentRepository.findAll();
+    }
+    @Override
+    public StudentEntity readStudent(Long id) {
+        return studentRepository.findById(id).get();
+    }
+    @Override
+    public String deleteStudent(Long id) {
+        studentRepository.deleteById(id);
+        return "Студент удален из БД";
+    }
+    @Override
+    public String updateStudent(StudentEntity entity, Long id) {
+        StudentEntity studentEntity = studentRepository.findById(id).get();
+        studentEntity.setFaculty(entity.getFaculty());
+        studentEntity.setLogin(entity.getLogin());
+        studentEntity.setPassword(entity.getPassword());
+        
+        studentRepository.save(studentEntity);
+        return "Данные  студента обновлены";
+    }
 }
