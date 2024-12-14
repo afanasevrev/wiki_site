960e1c177f0e8e88442b43a087cae0f9ff36eab7
revoly afanasev <rewoly@mail.ru>
2024-12-14 11:20:02 +0900

    Работа с репозиторием материалов

--- Server/src/main/java/com/students/Server/Service/MaterialsService.java
+++ Server/src/main/java/com/students/Server/Service/MaterialsService.java
@@ -1,7 +1,13 @@
 package com.students.Server.Service;
 
+import com.students.Server.Entity.MaterialsEntity;
 import org.springframework.stereotype.Service;
+import java.util.List;
 @Service
 public interface MaterialsService {
-
+    String createMaterial(MaterialsEntity entity);
+    List<MaterialsEntity> readMaterial();
+    MaterialsEntity readMaterial(Long id);
+    String deleteMaterial(Long id);
+    String updateMaterial(MaterialsEntity entity, Long id);
 }
--- Server/src/main/java/com/students/Server/Service/MaterialsServiceImpl.java
+++ Server/src/main/java/com/students/Server/Service/MaterialsServiceImpl.java
@@ -1,8 +1,33 @@
 package com.students.Server.Service;
 
+import com.students.Server.Entity.MaterialsEntity;
 import com.students.Server.Repository.MaterialsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+/**
+ * Класс для работы с данными материалов
+ */
 public class MaterialsServiceImpl implements MaterialsService {
     @Autowired
     private MaterialsRepository materialsRepository;
+    @Override
+    public String createMaterial(MaterialsEntity entity) {
+        return null;
+    }
+    @Override
+    public List<MaterialsEntity> readMaterial() {
+        return null;
+    }
+    @Override
+    public MaterialsEntity readMaterial(Long id) {
+        return null;
+    }
+    @Override
+    public String deleteMaterial(Long id) {
+        return null;
+    }
+    @Override
+    public String updateMaterial(MaterialsEntity entity, Long id) {
+        return null;
+    }
 }
