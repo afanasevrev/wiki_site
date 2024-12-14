c51ccac40a2c592fa2c04d117aff942a04c31486
revoly afanasev <rewoly@mail.ru>
2024-12-14 11:09:56 +0900

    Работа с репозиторием администраторов

--- Server/src/main/java/com/students/Server/Service/AdminsService.java
+++ Server/src/main/java/com/students/Server/Service/AdminsService.java
@@ -1,6 +1,13 @@
 package com.students.Server.Service;
 
+import com.students.Server.Entity.AdminsEntity;
 import org.springframework.stereotype.Service;
+import java.util.List;
 @Service
 public interface AdminsService {
+    String createAdmin(AdminsEntity entity);
+    List<AdminsEntity> readAdmins();
+    AdminsEntity readAdmin(Long id);
+    String deleteAdmin(Long id);
+    String updateAdmin(AdminsEntity entity, Long id);
 }
--- Server/src/main/java/com/students/Server/Service/AdminsServiceImpl.java
+++ Server/src/main/java/com/students/Server/Service/AdminsServiceImpl.java
@@ -1,8 +1,44 @@
 package com.students.Server.Service;
 
+import com.students.Server.Entity.AdminsEntity;
+import com.students.Server.Entity.StudentEntity;
 import com.students.Server.Repository.AdminsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+/**
+ * Класс для операций над данными администраторов
+ */
 public class AdminsServiceImpl implements AdminsService {
     @Autowired
     private AdminsRepository adminsRepository;
+    @Override
+    public String createAdmin(AdminsEntity entity) {
+        adminsRepository.save(entity);
+        return "Администратор добавлен в БД";
+    }
+    @Override
+    public List<AdminsEntity> readAdmins() {
+        return adminsRepository.findAll();
+    }
+    @Override
+    public AdminsEntity readAdmin(Long id) {
+        return adminsRepository.findById(id).get();
+    }
+    @Override
+    public String deleteAdmin(Long id) {
+        adminsRepository.deleteById(id);
+        return "Администратор удалён из БД";
+    }
+    @Override
+    public String updateAdmin(AdminsEntity entity, Long id) {
+        AdminsEntity adminsEntity = adminsRepository.findById(id).get();
+        adminsEntity.setLogin(entity.getLogin());
+        adminsEntity.setPassword(entity.getPassword());
+        adminsEntity.setName(entity.getName());
+        adminsEntity.setSurname(entity.getSurname());
+        adminsEntity.setPatronymic(entity.getPatronymic());
+        adminsEntity.setPosition(entity.getPosition());
+        adminsRepository.save(adminsEntity);
+        return "Данные администратора обновлены";
+    }
 }
